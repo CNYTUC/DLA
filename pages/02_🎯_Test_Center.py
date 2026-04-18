f9a3c68658e7690a6cc9a0a8adcbf904af2d38d3
CNYTUC <cuneytuc@gmail.com>
2026-04-18 18:35:15 -0400

    dn

--- pages/02_🎯_Test_Center.py
+++ pages/02_🎯_Test_Center.py
@@ -27,10 +27,21 @@ if questions and current_index < len(questions):
     q = questions[current_index]
     question_id, q_category, q_subcategory, question_text, image_path, difficulty, active = q
 
-    st.markdown(f"### Question {current_index + 1}")
-    st.write(f"**Category:** {q_category}")
-    st.write(f"**Subcategory:** {q_subcategory}")
-    st.write(f"**Difficulty:** {difficulty}")
+
+    st.markdown("<p style='text-align:center; font-size:18px;'>"&
+                
+                "No:" & {current_index + 1} & 
+                ", Category:" & {q_category + 1} & 
+                ", Subcategory:" & {q_subcategory + 1} & 
+                ", Difficulty:" & {difficulty + 1} & 
+                
+                "</p>", unsafe_allow_html=True)
+
+
+    # st.markdown(f"### Question {current_index + 1}")
+    # st.write(f"**Category:** {q_category}")
+    # st.write(f"**Subcategory:** {q_subcategory}")
+    # st.write(f"**Difficulty:** {difficulty}")
 
     if q_category == "Picture Description" and image_path:
         try:
